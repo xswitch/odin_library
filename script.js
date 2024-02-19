d1943dc514e4e7f4a11b8bb13ae2b4a1e198563d
xswitch <bivarmong@gmail.com>
2024-02-19 12:53:33 +0100

    Added method for adding library objects

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
-console.log('Hello world!');
+const myLibrary = [];
+
+function Book(name, author, pages, haveRead) {
+    this.name = name;
+    this.author = author;
+    this.pages = pages;
+    this.haveRead = haveRead
+}
+
+Book.prototype.logInfo = function() {
+    console.log([this.name, this.author, this.pages, this.haveRead]);
+}
+
+function addBookToLibrary(inputObj) {
+    const newBook = new Book(inputObj.name, inputObj.author, inputObj.pages, inputObj.read)
+    newBook.logInfo();
+    myLibrary.push(newBook);
+}
+
+const dummyInputObject = {
+    name: 'Night Angel',
+    author: 'Brent Weeks',
+    pages: 255,
+    read: true,
+}
+
+const dummyInputObject2 = {
+    name: 'Beer Crawler',
+    author: 'Homie simpsons',
+    pages: 35,
+    read: true,
+}
+
+addBookToLibrary(dummyInputObject)
+addBookToLibrary(dummyInputObject2)
